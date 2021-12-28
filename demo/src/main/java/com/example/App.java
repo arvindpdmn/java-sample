b829728bb78794c472da1163c4da12fabb89efd7
Arvind Padmanabhan <arvindpdmn@gmail.com>
2021-12-28 17:31:06 +0530

    Configured Log4j. Moved .gitignore file to top level.

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,13 +1,19 @@
 package com.example;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private static final Logger logger = LogManager.getLogger(App.class);
+
     public static void main( String[] args )
     {
+        logger.error("Hello Logger!");
         System.out.println( "Hello World!" );
     }
 }
